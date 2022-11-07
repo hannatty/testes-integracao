b23bea9a472d6dfe76f5a885c3f75af8a269a263
Hannatty <hanatxy@gmail.com>
2022-11-07 18:13:16 -0300

    testes de integração módulo 4

--- src/test/java/modulo4/IntegracaoTest04.java
+++ src/test/java/modulo4/IntegracaoTest04.java
@@ -16,6 +16,41 @@ import java.util.Arrays;
 import java.util.List;
 
 public class IntegracaoTest04 {
+    @Test
+    public void listarEstudantesFromPessoas(){
+        ListaPessoas listaPessoas = Mockito.mock(ListaPessoas.class);
+        Mockito.when(listaPessoas.getPessoas()).thenReturn(Arrays.asList(new Estudante(),new Estudante(),new Professor(),new Professor(),new Estudante(),new Gerente(),new Estudante(),new Estudante(),new Bibliotecario()));
+        List<Estudante> estudantes = new ArrayList<Estudante>();
 
+        listaPessoas.getPessoas().forEach(p -> {
+            if(p.getClass() == Estudante.class){
+                estudantes.add((Estudante) p);
+            }
+        });
 
+        Assertions.assertEquals(5, estudantes.size());
+    }
+
+    @Test
+    public void listarEstudanteInexistenteFromPessoas(){
+        ListaPessoas listaPessoas = Mockito.mock(ListaPessoas.class);
+        Mockito.when(listaPessoas.getPessoas()).thenReturn(Arrays.asList(new Professor(),new Professor(),new Gerente(),new Bibliotecario()));
+        List<Estudante> estudantes = new ArrayList<Estudante>();
+
+        listaPessoas.getPessoas().forEach(p -> {
+            if(p.getClass() == Estudante.class){
+                estudantes.add((Estudante) p);
+            }
+        });
+
+        Assertions.assertEquals(0, estudantes.size());
+    }
+
+    @Test
+    public void agruparLivrosByName(){
+        ListaLivros listaLivros = Mockito.mock(ListaLivros.class);
+        Mockito.when(listaLivros.groupNyName("Programando em Ruby on Rails")).thenReturn(Livro.findAll());
+        Assertions.assertEquals(0, listaLivros.groupNyName("Programando em Ruby on Rails").size());
+        Mockito.verify(listaLivros, Mockito.atMost(1)).getLivros();
+    }
 }
